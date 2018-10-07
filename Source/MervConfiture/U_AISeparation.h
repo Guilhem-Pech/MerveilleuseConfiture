2fb6cbe6912c86b5f435f87dcc3a5b0693512be9
Razania <j.ferreira1215@gmail.com>
2018-10-06 22:59:04 -0400

    Final commit pour IA

---
+++ Source/MervConfiture/U_AISeparation.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "U_AISeparation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MERVCONFITURE_API UU_AISeparation : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
